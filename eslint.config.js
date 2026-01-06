7e1de741e920f4370f4603694332b487686f1f7d
Eric Kimwatan <erickimwatan@microsoft.com>
2026-01-06 12:56:44 -0500

    Update eslint config and useBingoGame hook

--- eslint.config.js
+++ eslint.config.js
@@ -11,13 +11,34 @@ export default defineConfig([
     files: ['**/*.{ts,tsx}'],
     extends: [
       js.configs.recommended,
-      tseslint.configs.recommended,
+      ...tseslint.configs.recommendedTypeChecked,
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
     ],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+    rules: {
+      // Unused variables
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+          caughtErrorsIgnorePattern: '^_',
+        },
+      ],
+      // Require await in async functions
+      '@typescript-eslint/require-await': 'error',
+      // Disallow floating promises
+      '@typescript-eslint/no-floating-promises': 'error',
+      // Disallow misused promises
+      '@typescript-eslint/no-misused-promises': 'error',
     },
   },
 ])
